8b8007aad57a7d6d4f170c920d891498e5914951
IdekMan <61389691+CasualVideoGameEnjoyer@users.noreply.github.com>
2025-09-13 22:18:19 +0200

    Update IntercomPatch.cs
    Optimized the way team player counts were retrieved and used
    Made it so remaining time just uses Mathf.CeilToInt(floatTime).ToStri ng(); instead of whatever else was happening
    Removed the unnecessary static method for getting translated state

--- IntercomDisplayInfo/IntercomPatch.cs
+++ IntercomDisplayInfo/IntercomPatch.cs
@@ -27,31 +27,19 @@ namespace IntercomDisplayInfo
             if (icomInstance == null)
                 return;
             
-            var classD = Player.List.Count(p => p.Role == RoleTypeId.ClassD);
-            var fforces = Player.List.Count(p => p.Team == Team.FoundationForces);
-            var scientist = Player.List.Count(p => p.Role == RoleTypeId.Scientist);
-            var chaos = Player.List.Count(p => p.Team == Team.ChaosInsurgency);
-            var scps = Player.List.Count(p => p.Team == Team.SCPs);
-            
-            static string GetTranslatedState(IntercomState state)
-            {
-                var translation = IntercomDisplayInfo.Instance.Translation;
-                
-                return state switch
-                {
-                    IntercomState.Ready => translation.StateReady,
-                    IntercomState.Starting => translation.StateStarting,
-                    IntercomState.InUse => translation.StateInUse,
-                    IntercomState.Cooldown => translation.StateCooldown,
-                    IntercomState.NotFound => translation.StateNotFound,
-                    _ => state.ToString()
-                };
-            }
-
+            var translation = IntercomDisplayInfo.Instance.Translation;
             var intercomState = Intercom.State;
-            string translatedState = GetTranslatedState(intercomState);
-            var remainingTime = Mathf.Round(icomInstance.RemainingTime);
-            string intercomRemainingTime = remainingTime > 1 ? remainingTime.ToString() : "";
+            string translatedState = intercomState switch
+            {
+                IntercomState.Ready => translation.StateReady,
+                IntercomState.Starting => translation.StateStarting,
+                IntercomState.InUse => translation.StateInUse,
+                IntercomState.Cooldown => translation.StateCooldown,
+                IntercomState.NotFound => translation.StateNotFound,
+                _ => intercomState.ToString()
+            };
+            
+            var remainingTime = Mathf.CeilToInt(icomInstance.RemainingTime).ToString();
             string roundTime = $"{Round.Duration.TotalSeconds / 60:00}:{Round.Duration.TotalSeconds % 60:00}";
             
             StringBuilder stringBuilder = new StringBuilder();
@@ -61,28 +49,29 @@ namespace IntercomDisplayInfo
 
             stringBuilder.AppendLine($"<color=white>{IntercomDisplayInfo.Instance.Translation.Information}</color>");
             
+            
+            var teamCounts = Player.ReadyList.GroupBy(p => p.Team).ToDictionary(g => g.Key, g => (byte)g.Count());
+            
             if (IntercomDisplayInfo.Instance.Config.ClassD)
-                stringBuilder.AppendLine($"<color=#f7a71b>{IntercomDisplayInfo.Instance.Translation.ClassDText} - {classD}</color>");
+                stringBuilder.AppendLine($"<color=#f7a71b>{IntercomDisplayInfo.Instance.Translation.ClassDText} - {teamCounts.GetValueOrDefault(Team.ClassD)}</color>");
             
             if (IntercomDisplayInfo.Instance.Config.FForces)
-                stringBuilder.AppendLine($"<color=#3550db>{IntercomDisplayInfo.Instance.Translation.FoundationForces} - {fforces}</color>");
+                stringBuilder.AppendLine($"<color=#3550db>{IntercomDisplayInfo.Instance.Translation.FoundationForces} - {teamCounts.GetValueOrDefault(Team.FoundationForces)}</color>");
             
             if (IntercomDisplayInfo.Instance.Config.Scientist)
-                stringBuilder.AppendLine($"<color=#d4eb7a>{IntercomDisplayInfo.Instance.Translation.Scientists} - {scientist}</color>");
+                stringBuilder.AppendLine($"<color=#d4eb7a>{IntercomDisplayInfo.Instance.Translation.Scientists} - {teamCounts.GetValueOrDefault(Team.Scientists)}</color>");
             
             if (IntercomDisplayInfo.Instance.Config.Chaos)
-                stringBuilder.AppendLine($"<color=#227a0f>{IntercomDisplayInfo.Instance.Translation.ChaosInsurgency} - {chaos}</color>");
+                stringBuilder.AppendLine($"<color=#227a0f>{IntercomDisplayInfo.Instance.Translation.ChaosInsurgency} - {teamCounts.GetValueOrDefault(Team.ChaosInsurgency)}</color>");
             
             if (IntercomDisplayInfo.Instance.Config.Scps)
-                stringBuilder.AppendLine($"<color=#c21010>{IntercomDisplayInfo.Instance.Translation.SCPs} - {scps}</color>");
+                stringBuilder.AppendLine($"<color=#c21010>{IntercomDisplayInfo.Instance.Translation.SCPs} - {teamCounts.GetValueOrDefault(Team.SCPs)}</color>");
 
             stringBuilder.AppendLine("<color=white>-------------------------------------</color>");
             
             stringBuilder.AppendLine($"{IntercomDisplayInfo.Instance.Translation.State} - {translatedState}");
-            if (Intercom.State == IntercomState.InUse || Intercom.State == IntercomState.Cooldown)
-            {
-                stringBuilder.AppendLine($"{IntercomDisplayInfo.Instance.Translation.Timer} {intercomRemainingTime}");
-            }
+            if (intercomState is IntercomState.InUse or IntercomState.Cooldown)
+                stringBuilder.AppendLine($"{IntercomDisplayInfo.Instance.Translation.Timer} {remainingTime}");
             
             
             singletonInstance.Network_overrideText = stringBuilder.ToString();
